8edd3a210453592dea0959ac9be87f18d608b0a5
AliMoradpour <mr.mrdp@yahoo.com>
2023-10-13 15:41:33 +0330

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-500">
-      hello world
+    <h1 className="text-2xl text-teal-500 font-bold">
+      Hello world
     </h1>
   )
 }
